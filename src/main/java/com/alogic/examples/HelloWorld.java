e88344adc38fad2aa14f519975c783e21c42c7fa
laoqiming <qiminglao@qq.com>
2017-05-17 16:44:38 +0800

    helloworld例子

---
+++ src/main/java/com/alogic/examples/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.alogic.examples;
+
+import com.logicbus.backend.Context;
+import com.logicbus.backend.Servant;
+import com.logicbus.backend.message.RawMessage;
+
+public class HelloWorld extends Servant {
+
+	public int actionProcess(Context ctx) throws Exception {
+		
+		//获取服务调用消息
+		RawMessage msg = (RawMessage)ctx.asMessage(RawMessage.class);
+		
+		//获取输入输出缓冲区
+		StringBuffer buf = msg.getBuffer();
+		
+		//输出Helloworld到缓冲区
+		buf.setLength(0);
+		buf.append("Hello world");
+
+		return 0;
+	}
+}
